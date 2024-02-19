52af3bcff70691f084062bcffe8da282bc064441
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2024-02-19 10:19:16 -0300

    att

---
+++ Exercicios/src/Heranca/Polimorfismo/Exercicios/Mamifero.java
@@ -0,0 +1,36 @@
+package Heranca.Polimorfismo.Exercicios;
+
+public class Mamifero extends Animal{
+
+    private String alimento;
+    
+    public Mamifero() {
+        super();
+        this.setCor("Castanho");
+        this.setAmbiente("Terra");
+        this.alimento = "Mel";
+    }
+
+    /**
+     * @return the alimento
+     */
+    public String getAlimento() {
+        return alimento;
+    }
+
+    /**
+     * @param alimento the alimento to set
+     */
+    public void setAlimento(String alimento) {
+        this.alimento = alimento;
+    }
+
+    @Override
+    public String toString() {
+        String s = super.toString();
+        s += "\nAlimento: " + alimento;
+        return s;
+    }
+    
+    
+}
---
+++ Exercicios/src/Heranca/Polimorfismo/Exercicios/Peixe.java
@@ -0,0 +1,37 @@
+package Heranca.Polimorfismo.Exercicios;
+
+public class Peixe extends Animal{
+
+    private String caracteristicas;
+    
+    public Peixe() {
+        super();
+        this.setPatas(0);
+        this.setAmbiente("Mar");
+        this.setCor("Cinzenta");
+        this.caracteristicas = "Barbatanas e cauda";
+    }
+
+    /**
+     * @return the caracteristicas
+     */
+    public String getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    /**
+     * @param caracteristicas the caracteristicas to set
+     */
+    public void setCaracteristicas(String caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+
+    @Override
+    public String toString() {
+        String s = super.toString();
+        s += "\nCaracterísticas: " + caracteristicas;
+        return s;
+    }
+    
+    
+}
