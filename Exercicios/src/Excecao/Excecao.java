7acdb9f1738d4e48cbf7ee3f34a72c11ac4d9c79
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2024-03-05 11:11:40 -0300

    TryCatch

--- Exercicios/src/Excecao/Excecao.java
+++ Exercicios/src/Excecao/Excecao.java
@@ -8,7 +8,7 @@ public class Excecao {
 			int[] vetor = new int[4];
 
 			System.out.println("Antes da exception");
-			
+			 
 			vetor[4] = 1;
 			
 			System.out.println("Esse texto não será impresso");
---
+++ Exercicios/src/Excecao/MultiplosCatch/MultiplosCatchJava.java
@@ -0,0 +1,22 @@
+package Excecao.MultiplosCatch;
+
+/*Funciona apenas em java 7 pra cima*/
+
+public class MultiplosCatchJava {
+
+	public static void main(String[] args) {
+
+		int[] numeros = {4, 8, 16, 32, 64, 128};
+		int[] demon = {2, 0, 4, 8, 0};
+
+		for (int i=0; i<numeros.length; i++){
+			try{
+				System.out.println(numeros[i] + "/" + demon[i] + " = " + (numeros[i]/demon[i]));
+			}
+			catch(ArithmeticException | ArrayIndexOutOfBoundsException  e){
+				System.out.println("Aconteceu um erro");
+			}
+		}
+
+	}
+}
