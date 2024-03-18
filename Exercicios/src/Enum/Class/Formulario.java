1515d99e34f90ddccd137221bf87e6b9a9bf0009
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2024-03-18 16:41:32 -0300

    Enum

---
+++ Exercicios/src/Enum/Class/Formulario.java
@@ -0,0 +1,17 @@
+package Enum.Class;
+
+public class Formulario {
+	
+	enum Genero {
+		FEMININO('F'), MASCULINO('M');
+		
+		private char valor;
+		
+		Genero(char valor){
+			this.valor = valor;
+		}
+	}
+
+	private String nome;
+	private Genero genero;
+}
---
+++ Exercicios/src/Enum/Value/TesteEnum2.java
@@ -0,0 +1,16 @@
+package Enum.Value;
+
+import Enum.Class.DiaSemana;
+
+public class TesteEnum2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Enum.valueOf(DiaSemana.class, "DOMINGO"));
+		
+		DiaSemana dia = Enum.valueOf(DiaSemana.class, "DOMINGO");
+		
+		System.out.println(dia);
+	}
+
+}
