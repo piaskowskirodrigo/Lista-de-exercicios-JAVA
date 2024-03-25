7ef580e6aa8d2e0d5a973cd2998e0a58734f812c
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2024-03-25 15:16:59 -0300

    Vetor Part2

---
+++ Exercicios/src/VetorParte2/Teste/Teste02.java
@@ -0,0 +1,13 @@
+package VetorParte2.Teste;
+
+import VetorParte2.Vetor;
+
+public class Teste02 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(5);
+
+	}
+
+}
---
+++ Exercicios/src/VetorParte2/Teste/Teste03.java
@@ -0,0 +1,15 @@
+package VetorParte2.Teste;
+
+import VetorParte2.Vetor;
+
+public class Teste03 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(2);
+
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+	}
+}
