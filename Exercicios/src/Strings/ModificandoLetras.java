f78486ee38ea39fc0bbab31dd54334107a715dad
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2024-04-20 13:53:06 -0300

    String

---
+++ Exercicios/src/Strings/ModificandoLetras.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class ModificandoLetras {
+	public static void main(String[] args) {
+		
+		String teste = "Teste";
+		
+		String testeMinusc = teste.toLowerCase();
+		String testeMaisc = teste.toUpperCase();
+
+		System.out.println(testeMinusc);
+		System.out.println(testeMaisc);
+		
+		//if (teste.toLowerCase().equals(teste.toLowerCase()))
+	}
+
+}
