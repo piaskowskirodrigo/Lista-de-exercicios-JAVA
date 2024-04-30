962d65530af88d73ced562236052d33882396d1f
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2024-04-30 19:10:28 -0300

    Date

---
+++ Exercicios/src/ClassesUtilitarias/ClassTimeZone.java
@@ -0,0 +1,37 @@
+package ClassesUtilitarias;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class ClassTimeZone {
+	public static void main(String[] args) {
+		
+		Calendar calendar = Calendar.getInstance();
+		TimeZone tz = calendar.getTimeZone();
+		
+		System.out.println(tz); // EST
+		
+//		String[] fusos = TimeZone.getAvailableIDs();
+//		for (String fuso: fusos) {
+//			System.out.println(fuso);
+		
+		TimeZone tzSP = TimeZone.getTimeZone("America/Sao_Paulo");
+		
+		System.out.println(tzSP.getDisplayName());
+		System.out.println(tzSP.getID());
+
+		
+		Calendar agora = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss a z");
+		System.out.println(sdf.format(agora.getTime()));
+		
+		Calendar agoraSP = Calendar.getInstance(tzSP);
+		System.out.println(agoraSP.getTimeZone());
+		System.out.println(sdf.format(agoraSP.getTime()));
+		
+		agoraSP.add(Calendar.HOUR_OF_DAY, tzSP.getOffset((System.currentTimeMillis())) / 1000 / 60/ 60);
+		System.out.println(sdf.format(agoraSP.getTime()));
+	}
+
+}
